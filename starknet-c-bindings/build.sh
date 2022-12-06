105bcbebaaf328fb93346970bc68371e841cefa0
Bartosz Rybarski <bartosz.rybarski@swmansion.com>
2022-12-06 15:12:14 +0100

    Update build script

--- starknet-c-bindings/build.sh
+++ starknet-c-bindings/build.sh
@@ -2,16 +2,14 @@
 
 cbindgen --lang c --output include/crypto-rs.h
 
-ios_targets=(
+targets=(
   "aarch64-apple-ios"
-)
-
-iossimulator_targets=(
   "aarch64-apple-ios-sim"
   "x86_64-apple-ios"
 )
 
-targets+=( "${ios_targets[@]}" "${iossimulator_targets[@]}" )
+mkdir -p binaries
+mkdir -p frameworks
 
 for target in "${targets[@]}"; do
   rustup target add ${target}
@@ -21,17 +19,17 @@ done
 lipo -create \
   ../target/x86_64-apple-ios/release/libstarknet_c_bindings.a \
   ../target/aarch64-apple-ios-sim/release/libstarknet_c_bindings.a \
-  -output libstarknet_c_bindings_iossimulator.a
+  -output binaries/libstarknet_c_bindings_iossimulator.a
 
 lipo -create \
   ../target/aarch64-apple-ios/release/libstarknet_c_bindings.a \
-  -output libstarknet_c_bindings_ios.a
+  -output binaries/libstarknet_c_bindings_ios.a
 
-rm -r CryptoRs.xcframework
+rm -r frameworks/CryptoRs.xcframework
 
 xcodebuild -create-xcframework \
-  -library ./libstarknet_c_bindings_iossimulator.a \
+  -library ./binaries/libstarknet_c_bindings_iossimulator.a \
   -headers include/ \
-  -library ./libstarknet_c_bindings_ios.a \
+  -library ./binaries/libstarknet_c_bindings_ios.a \
   -headers include/ \
-  -output CryptoRs.xcframework
+  -output frameworks/CryptoRs.xcframework
